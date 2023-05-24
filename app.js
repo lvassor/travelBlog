1a934b945d16d761fdcffe67e5027a7e0b0d51b5
Luke Vassor <luke.vassor@avanade.com>
2023-05-24 10:12:42 +0100

    Making destinations page and first article for philippines

---
+++ app.js
@@ -0,0 +1,31 @@
+//jshint:esversion 6
+
+const express = require("express");
+const app = express();
+
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "/public")));
+
+app.get("/", function(req, res){
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+// Nav Bar
+app.get("/countries", function(req, res){
+    res.sendFile(path.join(__dirname, "public", "pages", "countries.html"));
+});
+app.get("/tips", function(req, res){
+    res.sendFile(path.join(__dirname, "public", "pages", "tips.html"));
+});
+app.get("/about", function(req, res){
+    res.sendFile(path.join(__dirname, "public", "pages", "about.html"));
+});
+
+// Countries
+app.get("/countries/philippines", function(req, res){
+    res.sendFile(path.join(__dirname, "public", "pages", "countries", "philippines", "philippines.html"));
+});
+
+app.listen(3000, function(){
+    console.log("Listening on port 3000");
+});
